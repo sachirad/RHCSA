8d1c4438af70fa36ac6285dcd89f4bf6d87851b4
Sachira Demein <149954182+sachirad@users.noreply.github.com>
2025-03-21 14:04:54 +0530

    Create sysuser.sh

---
+++ sysuser.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "================================================================="
+echo "================================================================="
+
+echo ""
+echo ""
+echo -n "Host name: "
+hostname
+
+echo ""
+echo ""
+echo "System Information: "
+echo ""
+uname -a
+echo ""
+echo ""
+
+echo "Users Currently Logged in: "
+users
+echo ""
